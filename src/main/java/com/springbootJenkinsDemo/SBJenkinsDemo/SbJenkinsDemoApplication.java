9a4a71ea72656e8fe185caa3c6d3d7f1cfcbdab6
za947017 <za947017@gmail>
2023-01-12 15:03:13 +0530

    first-commit

---
+++ src/main/java/com/springbootJenkinsDemo/SBJenkinsDemo/SbJenkinsDemoApplication.java
@@ -0,0 +1,18 @@
+package com.springbootJenkinsDemo.SBJenkinsDemo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbJenkinsDemoApplication {
+
+	public static final Logger logger=LoggerFactory.getLogger(SbJenkinsDemoApplication.class);
+
+	public static void main(String[] args) {
+		logger.info("it is the continius integration");
+		SpringApplication.run(SbJenkinsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootJenkinsDemo/SBJenkinsDemo/SbJenkinsDemoApplicationTests.java
@@ -0,0 +1,22 @@
+package com.springbootJenkinsDemo.SBJenkinsDemo;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class SbJenkinsDemoApplicationTests {
+	public static final Logger logger= LoggerFactory.getLogger(SbJenkinsDemoApplication.class);
+
+	@Test
+	void contextLoads() {
+		logger.info("it is the test continius integration");
+		assertEquals(true,true);
+
+	}
+
+
+}
